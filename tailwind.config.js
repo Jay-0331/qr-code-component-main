14912a9525a92963edcb61739a1fb0a9a5d71d6d
jay-0331 <jayvekariya75@gmail.com>
2022-02-17 16:46:16 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./**/*.{html,css}"
+  ],
+  theme: {
+    
+    fontFamily: {
+      text: ['Outfit', 'sans-serif'],
+    },
+    extend:{
+      colors:{
+        gray: {
+          250: '#d6e2f0',
+        }
+      },
+      maxWidth: {
+        s: '375px',
+      },
+    },
+  },
+  plugins: [],
+}
